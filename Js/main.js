bf2bbd5d1330ac3abd944e8b500eea08b7d0f8de
Clinton <57812399+clinton9ice@users.noreply.github.com>
2020-06-27 00:28:30 +0100

    Update

--- Js/main.js
+++ Js/main.js
@@ -1,78 +1,40 @@
 "use strict"
-let i, notice =`<b>W</b>elcome to Legit Mall Mobile website. <br />
-This is a site where you have the opportunity to buy and  sell your own goods but be mindfull of your price rate cause if one of our Customer care finds out your products are expensive, we'll be forced to remove it. <br> You can as well register as an online marketer, you'll be paid based on reviews and sells we get from your link and referals from your own site as well.<br> If you have any issue/suggestion regarding any of our Products or policy.
+let i,
+ notice =`<b>W</b>elcome to Legit Mall Mobile website. <br />
+This is a site where you have the opportunity to buy and  sell your own goods but be mindfull of your price rate cause if one of our Customer care finds out your products are expensive, we'll be forced to remove it. <br> You can as well register as an online marketer, you'll be paid based on reviews and sells we get from your links.<br> If you have any issues or suggestion regarding any of our Products or policy.
 <br />
- <address> Email: arizonaviva4gmail.com password: Viva5678 <br> Phone:+2348086954746,  +2348169336721</address>`, index = 0;
- let menu = document.querySelector(".headerBars");
- let  socialContainer =document.querySelector(".social-icon");
- let iconBtn = document.querySelector(".iconBtn"); 
- let icon = document.querySelector(".icon"); 
- let lists = document.querySelector(".lists")
- let nextBtn = document.querySelector("#next")
- let prevBtn = document.querySelector("#prev")
- let slider = document.querySelector(".slide").children
- let products = document.querySelectorAll(".products")
+<h2> Contact Us</h2>
+ <address> Email: clintonUfere@gmail.com <br />Phone:+2348086954746,  +2348169336721</address>`, 
+ index = 0; 
 
-
- //social-plugin
- iconBtn.addEventListener("click", function(){
-   socialContainer.classList.toggle("active");
- });
- nextBtn.onclick = () =>{
-if(index >= slider.length-1){
-index = 0;
+ //data
+ let carouselData = {
+first_data:{
+carouselHeader: "Section-one div ",
+textContent: "Just tell me something nicer than your footer."
+}, 
+second_data: {
+  carouselHeader: "second-Text",
+  textContent: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit in aspernatur ."
+},
+third_data: {
+  carouselHeader: "third-Text",
+  textContent: "  Lorem ipsum dolor sit amet consectetur "
 }
-else{
-index++
-}
-clearInterval()
-sliderFunction()
  }
-prevBtn.onclick = () =>{
-if(index == 0){
-index = slider.length-1
-}else{
-index --
-}
-sliderFunction()
- }
- setInterval(() =>{
-  if(index >= slider.length-1){
-    index = 0;
-    }
-    else{
-    index++
-    }
-  sliderFunction()
- }, 6000);
-
- //Ads Slidder
- function sliderFunction(){
-  for(var i = 0; i < slider.length; i++){
-    slider[i].classList.remove("active")
-    }
-    slider[index].classList.add("active")
- }
-//Mobile Menu 
- function show(event){
-  event.classList.toggle("active");
-  menu.onclick = element =>{
-    show(lists)
-    show(menu)
-    if(event.target == document.body){
-      menu.style.width = "0";
-      }
-    }
-}
-
-window.addEventListener("load", () =>{
-  loader()
+$(document).ready(function(){
+$(function(){
+  let loader_container, loaderChild, p;
+  loader_container = document.createElement("div")
+  loaderChild = document.createElement('img')
+  loader_container.setAttribute("class", "loaderContainer");
+  loaderChild.setAttribute("src", "https://i.ibb.co/TqWSxNd/Spinner-1s-200px.gif")
+  loader_container.appendChild(loaderChild)
+document.body.appendChild(loader_container)
 })
-
-
 //prompt Box
 function popBox(e){
-  setTimeout( () => {
+  setTimeout(() => {
   let popBox = document.createElement("div")
   let para = document.createElement("p")
   let header = document.createElement("h2")
@@ -95,34 +57,84 @@ function popBox(e){
 },10000)
   }
 
-//Navscroll Trigger
-window.onscroll = () =>{
-  let scrollheight = window.scrollY || document.body.innerHeight|| document.documentElement.innerHeight;
-if(Math.round(scrollheight) >= 300){
-document.querySelector("nav").classList.add("active")
+  //Hamburgger Menu
+$('.headerBars').click(function(){
+$(".lists").toggleClass("active")
+$(this).toggleClass("active")
+})
+
+//IconBtn
+$(".iconBtn").click(function(){
+  $("#social-icon").toggleClass("active")
+  $(".icon").toggleClass("active")
+})
+//carousel Section
+let slider = document.querySelector(".slide").children
+let carousel = function carouselFunction(){
+  if(index >= slider.length-1){
+    index = 0;
+    }
+    else{
+    index++
+    }
+    sliderFunction()
 }
-else{
-  document.querySelector("nav").classList.remove("active")
-}
+//Carousel-config-data
+$(function(){
+  //header
+  $('#first-carousel-data span .carousel-hd').html(carouselData.first_data.carouselHeader)
+  //txt
+  $('#first-carousel-data span .txt').html(carouselData.first_data.textContent)
+  //second-col-header
+  $("#second-carousel-data span .carousel-hd").html(carouselData.second_data.carouselHeader)
+  //second-col-txt 
+  $("#second-carousel-data span .txt").html(carouselData.second_data.textContent)
+  //third-col-header
+  $("#third-carousel-data span .carousel-hd").html(carouselData.third_data.carouselHeader)
+  //third-col-txt 
+  $("#third-carousel-data span .txt").html(carouselData.third_data.textContent)
+
+})
+let interval = setInterval(carousel, 10000)
+function sliderFunction(){
+  for(var i = 0; i < slider.length; i++){
+    slider[i].classList.remove("active")
+    }
+    slider[index].classList.add("active")
+ }
+ $(".slide").hover(function(){
+   clearInterval(interval)
+ })
+$("#next").on({
+  click: function(){
+   $(carousel)
+  }
+})
+
+$("#prev").click(function(){
+  if(index == 0){
+    index = slider.length-1
+    }else{
+    index --
+    }
+    sliderFunction()
+})
+
+//scroll-events
+$(window).scroll(function(){
+  let scrollheight = window.scrollY
+  if(Math.round(scrollheight) >= 300){
+   $("nav").addClass("active")
+    }
+    else{
+    $("nav").removeClass("active")
+    }
+    
  if(Math.round(scrollheight) >= 900){
-  document.querySelector(".fixedIcon").style.display ="block";
+ $(".fixedIcon").css("display", "block");
 }
 else{
-  document.querySelector(".fixedIcon").style.display ="none"
+$(".fixedIcon").css("display", "none")
 }
-}
-
-//loader
-function loader(){
-  let loader_container, loaderChild, p;
-  loader_container = document.createElement("div")
-  loaderChild = document.createElement('img')
-  loader_container.setAttribute("class", "loaderContainer");
-  loaderChild.setAttribute("src", "./Img/Spinner-1s-200px.gif")
-  loader_container.appendChild(loaderChild)
-document.body.appendChild(loader_container)
-}
-
-
-
-
+})
+})
